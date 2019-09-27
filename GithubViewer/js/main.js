079094d0ba9c998766e2a6f7a0206dc4cc8a8369
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2019-09-27 16:38:23 +0900

    ・GithubAPIの結果を表形式で描画する。

--- GithubViewer/js/main.js
+++ GithubViewer/js/main.js
@@ -28,6 +28,49 @@ function searchRepository(forks_count) {
 
 function updateTable(repository) {
 
+    // getElementByIdは、要素のIDを元に参照する。
+    var preTable = document.getElementById("github");
+    if (preTable != null) {
+        // removeChildは、呼び出し元の子要素を削除する。
+        document.getElementById("wrap_table").removeChild(preTable);
+    }
+
+    var items = repository["items"];
+    if (items == null) {
+        console.log("item項目無し");
+        return;
+    }
+
+    var wrapTable = document.getElementById("wrap_table");
+    // createElementは、指定の名前の要素を作成する。
+    var table = document.createElement("table");
+    // setAttributeは、要素に属性（属性名と値）を設定する。
+    table.setAttribute("id", "github");
+    // appendChildは、呼び出し元に要素を子要素として追加する。
+    wrapTable.appendChild(table);
+    var headTr = document.createElement("tr");
+    var headIdTh = document.createElement("th");
+    // textContentは、要素の本体を参照・設定する。
+    headIdTh.textContent = "ID";
+    var headNameTh = document.createElement("th");
+    headNameTh.textContent = "Name";
+    headTr.appendChild(headIdTh);
+    headTr.appendChild(headNameTh);
+    table.appendChild(headTr);
+    for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        var id = item["id"];
+        var idTd = document.createElement("td");
+        idTd.textContent = id;
+        var name = item["name"];
+        var nameTd = document.createElement("td");
+        nameTd.textContent = name;
+        var tr = document.createElement("tr");
+        tr.appendChild(idTd);
+        tr.appendChild(nameTd);
+        table.append(tr);
+    }
+
 }
 
 window.addEventListener("load", function() {
@@ -35,7 +78,8 @@ window.addEventListener("load", function() {
     var reqButton = document.getElementById("req");
     
     reqButton.addEventListener("click", function() {
-        searchRepository(100000);
+        var forks_count = parseInt(document.getElementById("forks_count").value);
+        searchRepository(forks_count);
     });
 
 });
