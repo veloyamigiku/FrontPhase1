20abaf0c8dcc567605ad5aed01b084f54ee2fff4
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2019-10-07 18:12:52 +0900

    ・Todo削除ボタンをクリックすると、Todo一覧からTodoを削除する。

--- Todo/js/main.js
+++ Todo/js/main.js
@@ -6,16 +6,26 @@ function addTodo(todoIdx, content, todoList) {
     var todoElement = document.createElement("div");
     todoElement.setAttribute("class", "todo");
     todoElement.setAttribute("data-todo-idx", newTodoIdx);
+    
     var todoEditBtn = document.createElement("div");
     todoEditBtn.setAttribute("class", "todoEditBtn");
     todoEditBtn.setAttribute("data-todo-idx", newTodoIdx);
     todoEditBtn.textContent = "edit";
     todoElement.appendChild(todoEditBtn);
+    
+    // Todo要素を削除するボタンを作成する。
     var todoDeleteBtn = document.createElement("div");
     todoDeleteBtn.setAttribute("class", "todoDeleteBtn");
     todoDeleteBtn.setAttribute("data-todo-idx", newTodoIdx);
     todoDeleteBtn.textContent = "delete";
+    todoDeleteBtn.addEventListener("click", function(event) {
+        // 削除ボタンの親要素（Todo要素）を、Todo要素の親要素から削除する。
+        var todoListEle = document.getElementById("todoList");
+        var todoEle = event.target.parentNode;
+        todoListEle.removeChild(todoEle);
+    });
     todoElement.appendChild(todoDeleteBtn);
+    
     var todoContent = document.createElement("div");
     todoContent.setAttribute("class", "content");
     todoContent.setAttribute("data-todo-idx", newTodoIdx);
