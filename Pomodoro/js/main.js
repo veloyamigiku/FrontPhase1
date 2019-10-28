3c4d6f132c87b9b546ad1a443393c8c4627443d7
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2019-10-28 12:00:11 +0900

    ・タイマーの開始・終了の機能（画面更新を含む）を追加する。 ・タイマー設定・設定完了時の画面更新のロジックを追加する。

--- Pomodoro/js/main.js
+++ Pomodoro/js/main.js
@@ -7,10 +7,59 @@ var timerMm = null;
 var timerSs = null;
 var timerMmInput = null;
 var timerSsInput = null;
+var timerId = null;
+var timerLastSec = null;
+
+function fromMmssStrToSec(mmStr, ssStr) {
+    return parseInt(mmStr) * 60 + parseInt(ssStr);
+}
+
+function timer(sec, timerMm, timerSs) {
+    var new_sec = sec - 1;
+    console.log(new_sec);
+    var mm = parseInt(new_sec / 60);
+    var ss = new_sec - (mm * 60);
+    timerMm.textContent = mm < 10 ? "0" + mm : mm;
+    timerSs.textContent = ss < 10 ? "0" + ss : ss;
+    return new_sec;
+}
+
+function timeup(
+    timerMm,
+    timerMmInput,
+    timerSs,
+    timerSsInput,
+    timerStart,
+    timerStop,
+    timerClear,
+    timerSetup) {
+     // 残り時間の画面表示をクリアする。
+     timerMm.textContent = timerMmInput.value;
+     timerSs.textContent = timerSsInput.value;
+     timerStart.style.display = "block";
+     timerStop.style.display = "none";
+     timerClear.style.display = "block";
+     timerSetup.style.display = "block";
+     // タイマーの残り時間をクリアする。
+     return fromMmssStrToSec(timerMmInput.value, timerSsInput.value);
+}
+
+function notification(message) {
+    if ("Notification" in window) {
+        var permission = Notification.permission;
+        if (permission == "denied") {
+            return;
+        }
+        Notification
+            .requestPermission()
+            .then(function() {
+                var notification = new Notification(message);
+            })
+    }
+}
 
 // 画面表示後のイベント処理を定義する。
 window.addEventListener("load", function() {
-
 
     /* タイマー開始ボタンのイベント処理を定義する。 */
     timerStart = document.getElementById("timer-start");
@@ -19,6 +68,33 @@ window.addEventListener("load", function() {
         timerStop.style.display = "block";
         timerClear.style.display = "none";
         timerSetup.style.display = "none";
+
+        // タイマーを開始する。
+        timerId = setInterval(
+            function(){
+                // 残り時間の変数と、画面表示を更新する。
+                timerLastSec = timer(
+                    timerLastSec,
+                    timerMm,
+                    timerSs);
+                if (timerLastSec == 0) {
+                    // 通知を表示する。
+                    notification("time up");
+                    // 残り時間がなくなったらタイマーを停止する。
+                    clearInterval(timerId);
+                    timerLastSec = timeup(
+                        timerMm,
+                        timerMmInput,
+                        timerSs,
+                        timerSsInput,
+                        timerStart,
+                        timerStop,
+                        timerClear,
+                        timerSetup);
+                }
+            },
+            1000);
+
     });
 
     /* タイマー停止ボタンのイベント処理を定義する。 */
@@ -28,6 +104,9 @@ window.addEventListener("load", function() {
         timerStop.style.display = "none";
         timerClear.style.display = "block";
         timerSetup.style.display = "block";
+
+        // タイマーを停止する。
+        clearInterval(timerId);
     });
 
     /* タイマークリアボタンのイベント処理を定義する。 */
@@ -39,12 +118,16 @@ window.addEventListener("load", function() {
     timerClear.addEventListener("click", function() {
         timerMm.textContent = timerMmInput.value;
         timerSs.textContent = timerSsInput.value;
+        // タイマーの残り時間をクリアする。
+        timerLastSec = fromMmssStrToSec(timerMmInput.value, timerSsInput.value);
     });
 
     /* タイマー設定ボタンのイベント処理を定義する。 */
     timerSetup = document.getElementById("timer-setup");
     timerSetup.addEventListener("click", function() {
         
+        timerStart.style.display = "none";
+        timerClear.style.display = "none";
         timerSetup.style.display = "none";
         var timerSettingRows = document.getElementsByClassName("timer-setting-row");
         for (var i = 0; i < timerSettingRows.length; i++) {
@@ -57,12 +140,22 @@ window.addEventListener("load", function() {
     timerSetupDone = document.getElementById("timer-setup-done");
     timerSetupDone.addEventListener("click", function() {
         
+        timerStart.style.display = "block";
+        timerClear.style.display = "block";
         timerSetup.style.display = "block";
         var timerSettingRows = document.getElementsByClassName("timer-setting-row");
         for (var i = 0; i < timerSettingRows.length; i++) {
             timerSettingRows[i].style.display = "none";
         }
 
+        // タイマーの残り時間を設定する。
+        timerLastSec = fromMmssStrToSec(timerMmInput.value, timerSsInput.value);
+        timerMm.textContent = timerMmInput.value;
+        timerSs.textContent = timerSsInput.value;
+
     });
+
+    // タイマーの残り時間を初期設定する。
+    timerLastSec = fromMmssStrToSec(timerMmInput.value, timerSsInput.value);
 
 });
