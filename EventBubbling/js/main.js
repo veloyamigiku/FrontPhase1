6f681fc2a00f53dfa2fce7e32d347b0738d129d9
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2019-10-30 15:48:16 +0900

    ・イベント伝播のサンプルコードを追加する。

---
+++ EventBubbling/js/main.js
@@ -0,0 +1,53 @@
+window.addEventListener("load", function() {
+    var target1;
+    var target2;
+    var target3;
+
+    target1 = document.getElementById("target1");
+    target1.addEventListener(
+        "click",
+        function() {
+            console.log("cap_click_target1");
+        },
+        true);
+    target1.addEventListener(
+        "click",
+        function() {
+            console.log("bub_click_target1");
+        },
+        false);
+    target2 = document.getElementById("target2");
+    target2.addEventListener(
+        "click",
+        function(event) {
+            // stopPropagationは、本関数以外のロジックを実行して、イベント伝播を停止する。
+            // stopImmediatePropagationは、本関数以外のロジックを実行せず、イベント伝播を停止する。
+            event.stopPropagation();
+            console.log("cap_click_target2");
+        },
+        true);
+    target2.addEventListener(
+        "click",
+        function() {
+            console.log("bub_click_target2");
+        },
+        false);
+    
+    target3 = document.getElementById("target3");
+    target3.addEventListener(
+        "click",
+        function(event) {
+            console.log("cap_click_target3");
+        },
+        true);
+    target3.addEventListener(
+        "click",
+        function(event) {
+            console.log("bub_click_target3");
+            
+            // preventDefaultは、イベント発生元の動作（リンクのクリックによる画面遷移）をキャンセルする。
+            //event.preventDefault();
+        },
+        false);
+    
+});
