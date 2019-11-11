f74c4baeb77299f4edda5bcaceb1782b095a4a3e
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2019-11-11 16:18:31 +0900

    ・ローカルスコープの説明を修正する。変数宣言のパターンの違いをコメント付で追加する。 ・即時実行関数によるスコープのサンプルコードを追加する。

--- Scope/js/main.js
+++ Scope/js/main.js
@@ -5,18 +5,38 @@ console.log("globalVal1:" + globalVal1);
 // グローバルスコープ
 var globalVal1 = "globalVal1";
 
+
+// 即時実行関数によるスコープ
+(function() {
+    console.log("====即時実行関数のスコープ====")
+    var immediate = "immediate";
+    console.log("immediate:" + immediate);
+})();
+//console.log(":" + immediate);
+// Uncaught ReferenceError: immediate is not defined
+
 // 関数スコープ
 function sampleFunction1() {
     var funcVal1 = "funcVal1";
 
     // ローカルスコープ（ブロックスコープ）
-    // ES2015以前（ブラウザetc）は、ブロックスコープは無い。
+    // var宣言の変数は、ブロックスコープを無視して直近のスコープ（関数）に巻き上げられる。
     for (var blockVal2 = 0; blockVal2 < 3; blockVal2++) {
         console.log("====ローカルスコープ（ブロックスコープ）_1====");
         console.log("blockVal2:" + blockVal2);
         // blockVal2:0
         // blockVal2:1
         // blockVal2:2
+    }
+
+    // ローカルスコープ（ブロックスコープ）
+    // let宣言の変数は、ブロックスコープが適用される。
+    for (let blockVal3 = 0; blockVal3 < 3; blockVal3++) {
+        console.log("====ローカルスコープ（ブロックスコープ）_3====");
+        console.log("blockVal3:" + blockVal3);
+        // blockVal3:0
+        // blockVal3:1
+        // blockVal3:2
     }
 
     console.log("====関数スコープ_1====");
@@ -28,10 +48,12 @@ function sampleFunction1() {
     // blockVal1:3
     console.log("blockVal2:" + blockVal2);
     // blockVal2:3
+    //console.log("blockVal3:" + blockVal3);
+    // Uncaught ReferenceError: blockVal3 is not defined
 }
 
 // ローカルスコープ（ブロックスコープ）
-// ES2015以前（ブラウザetc）は、ブロックスコープは無い。
+// var宣言の変数は、ブロックスコープを無視して直近のスコープ（グローバル）に巻き上げられる。
 console.log("====ローカルスコープ（ブロックスコープ）_2====");
 for (var blockVal1 = 0; blockVal1 < 3; blockVal1++) {
     console.log("globalVal1:" + globalVal1);
@@ -47,6 +69,8 @@ for (var blockVal1 = 0; blockVal1 < 3; blockVal1++) {
     
     //console.log("blockVal2:" + blockVal2);
     // Uncaught ReferenceError: blockVal2 is not defined
+    //console.log("blockVal3:" + blockVal3);
+    // Uncaught ReferenceError: blockVal3 is not defined
 
 }
 
@@ -60,7 +84,10 @@ console.log("globalVal1:" + globalVal1);
 console.log("blockVal1:" + blockVal1);
 // blockVal1:3
 
+sampleFunction1();
+
 //console.log("blockVal2:" + blockVal2);
 // Uncaught ReferenceError: blockVal2 is not defined
 
-sampleFunction1();
+//console.log("blockVal3:" + blockVal3);
+// Uncaught ReferenceError: blockVal3 is not defined
